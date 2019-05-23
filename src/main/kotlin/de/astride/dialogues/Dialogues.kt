a7d485e6e0669bfafd2f8754906cef38e592e19f
LartyHD <lartyhd@gmail.com>
2019-05-23 19:59:32 +0200

    Added config and data runtime implementation in theory

--- src/main/kotlin/de/astride/dialogues/Dialogues.kt
+++ src/main/kotlin/de/astride/dialogues/Dialogues.kt
@@ -1,9 +1,15 @@
 package de.astride.dialogues
 
 import de.astride.dialogues.commands.Dialogue
+import de.astride.dialogues.data.dialogues
+import de.astride.dialogues.data.options
+import de.astride.dialogues.data.states
 import de.astride.dialogues.events.Speech
+import de.astride.dialogues.functions.configService
+import de.astride.dialogues.services.ConfigService
 import kotlinx.coroutines.InternalCoroutinesApi
 import net.darkdevelopers.darkbedrock.darkness.spigot.plugin.DarkPlugin
+import org.bukkit.plugin.ServicePriority
 
 /**
  * @author Lars Artmann | LartyHD
@@ -14,6 +20,18 @@ class Dialogues : DarkPlugin() {
 
     @InternalCoroutinesApi
     override fun onEnable(): Unit = onEnable {
+        server.servicesManager.register(
+            ConfigService::class.java,
+            ConfigService(dataFolder),
+            this,
+            ServicePriority.Normal
+        ) //Important for ConfigService.instance
+
+        options = configService.loadOptions().toSet().toMutableSet()
+        dialogues = configService.loadDialogues().map { it.key to it.value.toMutableList() }.toMap().toMutableMap()
+        states = configService.loadStates().map { it.key to it.value.toMutableMap() }.toMap().toMutableMap()
+
+
         Dialogue(this)
         Speech.setup(this)
     }
--- src/main/kotlin/de/astride/dialogues/data/Data.kt
+++ src/main/kotlin/de/astride/dialogues/data/Data.kt
@@ -8,21 +8,21 @@ import java.util.*
  * Created by Lars Artmann | LartyHD on 08.05.2019 00:49.
  * Current Version: 1.0 (08.05.2019 - 22.05.2019)
  */
-val dialogues: MutableMap<Int, MutableList<Dialogue>> = mutableMapOf() //EntityID, all dialogues
+var dialogues: MutableMap<Int, MutableList<Dialogue>> = mutableMapOf() //EntityID, all dialogues
 
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 22.05.2019 17:13.
  * Current Version: 1.0 (22.05.2019 - 23.05.2019)
  */
-val states: MutableMap<UUID, MutableMap<Int, String>> = mutableMapOf() //(Player)UUID, <EntityID, DialogueID>
+var states: MutableMap<UUID, MutableMap<Int, String>> = mutableMapOf() //(Player)UUID, <EntityID, DialogueID>
 
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 23.05.2019 05:45.
  * Current Version: 1.0 (23.05.2019 - 23.05.2019)
  */
-val options: MutableSet<Option> = mutableSetOf()
+var options: MutableSet<Option> = mutableSetOf()
 
 /**
  * @author Lars Artmann | LartyHD
